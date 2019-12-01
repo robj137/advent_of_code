25f2cc65c932410108ca03fd315ae535686db11d
Rob <rojohns@microsoft.com>
2019-12-01 12:56:55 -0800

    misspellings that didn't matter, but still bug me :)

--- 2019/day01.py
+++ 2019/day01.py
@@ -8,18 +8,18 @@ def get_data():
         lines = f.readlines()
     return [int(x.strip()) for x in lines]
 
-def get_naive_requried_fuel(mass):
+def get_naive_required_fuel(mass):
     return np.floor(mass/3) - 2
 
 def get_actual_required_fuel(mass, pre_mass = 0):
-    next_fuel = get_naive_requried_fuel(mass)
+    next_fuel = get_naive_required_fuel(mass)
     if next_fuel <= 0:
         return pre_mass
     return get_actual_required_fuel(next_fuel, pre_mass + next_fuel)
 
 def main():
     data = np.array(get_data())
-    fuel = int(sum(get_naive_requried_fuel(data)))
+    fuel = int(sum(get_naive_required_fuel(data)))
     print('Part 1: Naive fuel requirement: {}'.format(fuel))
 
     fuel_2 = int(sum([get_actual_required_fuel(x) for x in get_data()]))
