41aecf5ca0f43e7f46ccb07d885cb0e6f880e846
Rob <rojohns@microsoft.com>
2022-12-10 07:45:55 -0800

    Day 10

--- 2022/day08.py
+++ 2022/day08.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     X, Y = stand.shape
     
     visible = np.zeros((X, Y), dtype=bool) # boolean array for tree visibility
-    scenic = np.zeros((X, Y))
+    scenic = np.zeros((X, Y), dtype=int)
     
     for i in range(X):
         for j in range(Y):
@@ -31,8 +31,8 @@ if __name__ == '__main__':
             arrs.append(np.array(stand[i, j+1:])) # look down
             arrs.append(np.flip(np.array(stand[:i, j]))) # look left
             arrs.append(np.array(stand[i+1:, j])) # look right
-            visible[i, j] = np.sum([np.all( val > x) for x in arrs])
+            visible[i, j] = np.sum([np.all(val > x) for x in arrs])
             scenic[i, j] = np.prod([get_viewing_distance(val, x) for x in arrs])
     
-    print('part 1:', int(np.sum(visible)))
-    print('part 2:', int(np.max(scenic)))
+    print('part 1:', np.sum(visible))
+    print('part 2:', np.max(scenic))
---
+++ 2022/day10.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def get_data():
+    with open('inputs/day10.txt') as f:
+    #with open('inputs/day10.test.txt') as f:
+        lines = [x.strip() for x in f.readlines()]
+    return lines
+
+class Communicator:
+    def __init__(self, data):
+        self.register = 1
+        self.data = data
+        self.cycle = 0
+        self.instruction_index = -1
+        self.current_instruction = 'noop'
+        self.current_instruction_lifetime = 0
+        self.interesting_signal_strengths = []
+        self.crt = []
+
+    def increment_cycle(self):
+        # beginning
+        self.cycle += 1
+        if self.current_instruction_lifetime == 0:
+            if self.current_instruction[0:4] == 'addx':
+                self.register += int(self.current_instruction.split()[1])
+            self.instruction_index += 1
+            if self.instruction_index == len(self.data):
+                return 0
+            self.current_instruction = self.data[self.instruction_index]
+            self.current_instruction_lifetime = 1 if self.current_instruction == 'noop' else 2
+
+        # middle bit (during cycle)
+        if np.abs((self.cycle-1)%40 - self.register) <= 1:
+            self.crt.append('█')
+        else:
+            self.crt.append(' ')
+
+        self.current_instruction_lifetime -= 1
+        if self.cycle in [20, 60, 100, 140, 180, 220]:
+            self.interesting_signal_strengths.append(self.cycle * self.register)
+        
+        return 1
+
+if __name__ == '__main__':
+    data = get_data()
+    communicator = Communicator(data)
+    while communicator.instruction_index < len(communicator.data):
+        communicator.increment_cycle()
+
+    print('Part 1:', sum(communicator.interesting_signal_strengths))
+    print('Part 2:')
+    [print(''.join(x)) for x in np.array(communicator.crt).reshape(6,40)]
